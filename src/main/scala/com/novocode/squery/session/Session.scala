2dc5386778ad18499c3dc3f5b7a7301afbe5784b
Stefan Zeiger <szeiger@novocode.com>
2009-07-18 22:39:13 +0200

    Removed PreparedStatement pooling.
    This is already supported by JDBC, so use something like Apache DBCP if
    you need it and your database driver does not implement it directly.

--- src/main/scala/com/novocode/squery/session/Session.scala
+++ src/main/scala/com/novocode/squery/session/Session.scala
@@ -1,51 +1,22 @@
 package com.novocode.squery.session
 
-import scala.collection.mutable.{Map, Stack}
 import java.sql.PreparedStatement
-import com.novocode.squery.SQueryException
 
 /**
  * A database session which opens a connection and transaction on demand.
  */
 class Session private[squery] (fact: SessionFactory) {
 
-  private class StatementMap {
-    private val m = Map.empty[String, Stack[PreparedStatement]]
-    def add(sql: String, ps: PreparedStatement) =  m get sql match {
-      case Some(s) => s push ps
-      case _ => {
-        val s = new Stack[PreparedStatement]
-        s push ps
-        m += ((sql, s))
-      }
-    }
-    def remove(sql: String) = m get sql match {
-      case Some(s) => if(s.isEmpty) None else Some(s.pop)
-      case _ => None
-    }
-    def foreach(f: PreparedStatement => Unit) =
-      for(s <- m.values; ps <- s) f(ps);
-  }
-
-  private val freeStatements = new StatementMap
-  private val usedStatements = new StatementMap
-
   var open = false
   var doRollback = false
   lazy val conn = { open = true; fact.createConnection() }
 
-  private[squery] def allocPS(sql: String) = freeStatements remove sql match {
-    case Some(ps) => { usedStatements add (sql, ps); ps }
-    case _ => conn.prepareStatement(sql)
-  }
+  private[squery] def allocPS(sql: String) = conn.prepareStatement(sql)
 
-  private[squery] def freePS(sql: String, ps: PreparedStatement) =
-    if(!ps.isClosed) freeStatements add (sql, ps)
+  private[squery] def freePS(sql: String, ps: PreparedStatement) = ps.close()
 
   def close() {
-    freeStatements foreach (_.close)
-    usedStatements foreach (_.close)
-    if(open) conn.close
+    if(open) conn.close()
   }
 
   /**
