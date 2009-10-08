2c72ad03e370606c220f1e3f7ed0d6c268901aaf
Stefan Zeiger <szeiger@novocode.com>
2009-10-08 21:54:12 +0200

    Updated build process: Building and publishing with sbt trunk is now working for all artifacts

--- project/build/src/ScalaQueryProject.scala
+++ project/build/src/ScalaQueryProject.scala
@@ -7,7 +7,7 @@ class ScalaQueryProject(info: ProjectInfo) extends DefaultProject(info)
   override def compileOptions = Deprecation :: super.compileOptions.toList
 
   /*********** Dependencies ***********/
-  val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+  //val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
   //val specs = "org.scala-tools.testing" % "specs" % "1.4.4"
   //val scalaCheck = "org.scalacheck" % "scalacheck" % "1.5"
   val h2 = "com.h2database" % "h2" % "1.1.+" % "test->default"
@@ -24,19 +24,17 @@ class ScalaQueryProject(info: ProjectInfo) extends DefaultProject(info)
   }) else super.fork
 
   /*********** Publishing ***********/
-  /*override def ivyXML =
-    <info module="scala-query" organisation="com.novocode">
-      <license name="Two-clause BSD-style" url="http://github.com/szeiger/scala-query/blob/master/LICENSE.txt" />
-      <description homepage="http://github.com/szeiger/scala-query/">
-        ScalaQuery is a type-safe database query API for Scala.
-      </description>
-    </info>*/
+  def specificSnapshotRepo =
+    Resolver.url("scala-nightly").
+    artifacts("http://scala-tools.org/repo-snapshots/[organization]/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]").
+    mavenStyle()
+  val nightlyScala = ModuleConfiguration("org.scala-lang", "*", "2.8.0-.*", specificSnapshotRepo)
+  //override def deliverScalaDependencies = Nil
   override def managedStyle = ManagedStyle.Maven
   val publishTo = Resolver.file("ScalaQuery Test Repo", new File("e:/temp/repo/"))
-  override def packageDocsJar = defaultJarPath("-javadoc.jar")
+  override def packageDocsJar = defaultJarPath("-javadocs.jar")
   override def packageSrcJar = defaultJarPath("-sources.jar")
   val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
   val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadocs"), Nil, None)
-  //override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
 }
