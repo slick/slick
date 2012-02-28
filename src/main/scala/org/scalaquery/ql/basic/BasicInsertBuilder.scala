d9282e030ae47038565cf1e6beaefeaa74c57c73
Chris Stivers <chris@stivers.us>
2012-02-28 20:51:04 +0900

    Enabled insert into a table identified by 'schemaName.tableName'

--- src/main/scala/org/scalaquery/ql/basic/BasicInsertBuilder.scala
+++ src/main/scala/org/scalaquery/ql/basic/BasicInsertBuilder.scala
@@ -10,37 +10,53 @@ class BasicInsertBuilder(val column: Any, val profile: BasicProfile) {
   import profile.sqlUtils._
 
   def buildInsert: String = {
-    val (table, cols, vals) = buildParts
-    "INSERT INTO " + quoteIdentifier(table) + " (" + cols + ") VALUES (" + vals + ")"
+    val (schema, table, cols, vals) = buildParts
+    
+    val insertInto = schema.map(quoteIdentifier(_) + "." + quoteIdentifier(table)).getOrElse(quoteIdentifier(table))
+    
+    "INSERT INTO " + insertInto + " (" + cols + ") VALUES (" + vals + ")"
   }
 
   def buildInsert(query: Query[_, _]): SQLBuilder.Result = {
-    val (table, cols, _) = buildParts
+    val (schema, table, cols, _) = buildParts
     val b = new SQLBuilder
-    b += "INSERT INTO " += quoteIdentifier(table) += " (" += cols.toString += ") "
+
+    b += "INSERT INTO "
+    
+    schema.foreach(b += quoteIdentifier(_) += '.')
+    b += quoteIdentifier(table)
+
+    b += " (" += cols.toString += ") "
+
     val qb = profile.createQueryBuilder(query, NamingContext())
     qb.buildSelect(b)
     b.build
   }
 
-  protected def buildParts: (String, StringBuilder, StringBuilder) = {
+  protected def buildParts: (Option[String],String, StringBuilder, StringBuilder) = {
     val cols = new StringBuilder
     val vals = new StringBuilder
     var table:String = null
+    var schema:Option[String] = None
     def f(c: Any): Unit = c match {
       case p:Projection[_] =>
         for(i <- 0 until p.productArity)
           f(Node(p.productElement(i)))
       case t:AbstractTable[_] => f(Node(t.*))
       case n:NamedColumn[_] =>
-        if(table eq null) table = n.table.asInstanceOf[AbstractTable[_]].tableName
-        else if(table != n.table.asInstanceOf[AbstractTable[_]].tableName) throw new SQueryException("Inserts must all be to the same table")
+        if(table eq null) {
+          table = n.table.asInstanceOf[AbstractTable[_]].tableName
+          schema = n.table.asInstanceOf[AbstractTable[_]].schemaName
+        }
+        else if(table != n.table.asInstanceOf[AbstractTable[_]].tableName) 
+          throw new SQueryException("Inserts must all be to the same table")
         appendNamedColumn(n, cols, vals)
       case _ => throw new SQueryException("Cannot use column "+c+" in INSERT statement")
     }
     f(Node(column))
-    if(table eq null) throw new SQueryException("No table to insert into")
-    (table, cols, vals)
+    if(table eq null) 
+      throw new SQueryException("No table to insert into")
+    (schema, table, cols, vals)
   }
 
   protected def appendNamedColumn(n: NamedColumn[_], cols: StringBuilder, vals: StringBuilder) {
