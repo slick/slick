774e347270010f361b2afa3321a001f0a162fd35
Stefan Zeiger <szeiger@novocode.com>
2012-06-15 11:40:51 +0200

    Make ReadAheadIterator a BufferedIterator and rename peek() to headOption

--- src/main/scala/scala/slick/util/ReadAheadIterator.scala
+++ src/main/scala/scala/slick/util/ReadAheadIterator.scala
@@ -4,7 +4,7 @@ package scala.slick.util
  * An iterator on top of a data source which does not offer a hasNext()
  * method without doing a next()
  */
-trait ReadAheadIterator[+T] extends Iterator[T] {
+trait ReadAheadIterator[+T] extends BufferedIterator[T] {
 
   private[this] var state = 0 // 0: no data, 1: cached, 2: finished
   private[this] var cached: T = null.asInstanceOf[T]
@@ -17,7 +17,16 @@ trait ReadAheadIterator[+T] extends Iterator[T] {
   /** Return a new value or call finished() */
   protected def fetchNext(): T
 
-  def peek(): Option[T] = {
+  def head: T = {
+    update()
+    if(state == 1) cached
+    else throw new NoSuchElementException("head on empty iterator")
+  }
+
+  @deprecated("Use headOption instead of peek()", "0.10.0-M2")
+  def peek(): Option[T] = headOption
+
+  def headOption: Option[T] = {
     update()
     if(state == 1) Some(cached)
     else None
