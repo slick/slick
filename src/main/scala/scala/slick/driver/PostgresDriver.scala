5256e3c6f16d21761d00574d7b1b64908f5983d0
Stefan Zeiger <szeiger@novocode.com>
2012-09-11 17:25:59 +0200

    Add support for OID-based BLOBs in PostgreSQL

--- src/main/scala/scala/slick/driver/PostgresDriver.scala
+++ src/main/scala/scala/slick/driver/PostgresDriver.scala
@@ -8,13 +8,17 @@ import scala.slick.ast.{SequenceNode, Library, FieldSymbol, Node}
 /**
  * Slick driver for PostgreSQL.
  *
- * This driver implements the [[scala.slick.driver.ExtendedProfile]]
- * ''without'' the following capabilities:
+ * This driver implements all capabilities of the
+ * [[scala.slick.driver.ExtendedProfile]].
+ *
+ * Notes:
  *
  * <ul>
  *   <li>[[scala.slick.driver.BasicProfile.capabilities.typeBlob]]:
- *   Blobs are not supported (but binary data in the form of
- *   <code>Array[Byte]</code> is).</li>
+ *   The default implementation of the <code>Blob</code> type uses the
+ *   database type <code>lo</code> and the stored procedure
+ *   <code>lo_manage</code>, both of which are provided by the "lo"
+ *   extension in PostgreSQL.</li>
  * </ul>
  *
  * @author szeiger
@@ -23,11 +27,8 @@ trait PostgresDriver extends ExtendedDriver { driver =>
 
   override val typeMapperDelegates = new TypeMapperDelegates
   override def createQueryBuilder(input: QueryBuilderInput): QueryBuilder = new QueryBuilder(input)
+  override def createTableDDLBuilder(table: Table[_]): TableDDLBuilder = new TableDDLBuilder(table)
   override def createColumnDDLBuilder(column: FieldSymbol, table: Table[_]): ColumnDDLBuilder = new ColumnDDLBuilder(column)
-
-  override val capabilities: Set[Capability] = (BasicProfile.capabilities.all
-    - BasicProfile.capabilities.typeBlob
-  )
 
   override def mapTypeName(tmd: TypeMapperDelegate[_]): String = tmd.sqlType match {
     case java.sql.Types.DOUBLE => "DOUBLE PRECISION"
@@ -51,6 +52,19 @@ trait PostgresDriver extends ExtendedDriver { driver =>
     }
   }
 
+  class TableDDLBuilder(table: Table[_]) extends super.TableDDLBuilder(table) {
+    override def createPhase1 = super.createPhase1 ++ columns.flatMap {
+      case cb: ColumnDDLBuilder => cb.createLobTrigger(table.tableName)
+    }
+    override def dropPhase1 = {
+      val dropLobs = columns.flatMap {
+        case cb: ColumnDDLBuilder => cb.dropLobTrigger(table.tableName)
+      }
+      if(dropLobs.isEmpty) super.dropPhase1
+      else Seq("delete from "+quoteIdentifier(table.tableName)) ++ dropLobs ++ super.dropPhase1
+    }
+  }
+
   class ColumnDDLBuilder(column: FieldSymbol) extends super.ColumnDDLBuilder(column) {
     override def appendColumn(sb: StringBuilder) {
       sb append quoteIdentifier(column.name) append ' '
@@ -61,17 +75,42 @@ trait PostgresDriver extends ExtendedDriver { driver =>
       else sb append sqlType
       appendOptions(sb)
     }
+
+    def lobTrigger(tname: String) =
+      quoteIdentifier(tname+"__"+quoteIdentifier(column.name)+"_lob")
+
+    def createLobTrigger(tname: String): Option[String] =
+      if(sqlType == "lo") Some(
+        "create trigger "+lobTrigger(tname)+" before update or delete on "+
+        quoteIdentifier(tname)+" for each row execute procedure lo_manage("+quoteIdentifier(column.name)+")"
+      ) else None
+
+    def dropLobTrigger(tname: String): Option[String] =
+      if(sqlType == "lo") Some(
+        "drop trigger "+lobTrigger(tname)
+      ) else None
   }
 
   class TypeMapperDelegates extends super.TypeMapperDelegates {
+    override val blobTypeMapperDelegate = new BlobTypeMapperDelegate
+    override val byteTypeMapperDelegate = new ByteTypeMapperDelegate
+    override val byteArrayTypeMapperDelegate = new ByteArrayTypeMapperDelegate
+    override val uuidTypeMapperDelegate = new UUIDTypeMapperDelegate
+
+    class BlobTypeMapperDelegate extends super.BlobTypeMapperDelegate {
+      override val sqlTypeName = "lo"
+    }
+
     /* PostgreSQL does not have a TINYINT type, so we use SMALLINT instead. */
-    override val byteTypeMapperDelegate = new ByteTypeMapperDelegate {
+    class ByteTypeMapperDelegate extends super.ByteTypeMapperDelegate {
       override def sqlTypeName = "SMALLINT"
     }
-    override val byteArrayTypeMapperDelegate = new ByteArrayTypeMapperDelegate {
+
+    class ByteArrayTypeMapperDelegate extends super.ByteArrayTypeMapperDelegate {
       override val sqlTypeName = "BYTEA"
     }
-    override val uuidTypeMapperDelegate = new UUIDTypeMapperDelegate {
+
+    class UUIDTypeMapperDelegate extends super.UUIDTypeMapperDelegate {
       override def setValue(v: UUID, p: PositionedParameters) = p.setObject(v, sqlType)
       override def setOption(v: Option[UUID], p: PositionedParameters) = p.setObjectOption(v, sqlType)
       override def nextValue(r: PositionedResult) = r.nextObject().asInstanceOf[UUID]
