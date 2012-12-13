9a3d7185f630c69cee32967c107da9fa4e00f213
Stefan Zeiger <szeiger@novocode.com>
2012-12-13 17:19:56 +0100

    Add "Self" type to Node for better return types.
    Now the methods that return copies of a node with some changes can have
    a more appropriate return type than just Node.

--- src/main/scala/scala/slick/direct/SlickBackend.scala
+++ src/main/scala/scala/slick/direct/SlickBackend.scala
@@ -20,16 +20,18 @@ trait QueryableBackend
 object CustomNodes{
   import scala.slick.ast._
   final case class Reverse(value: Node) extends UnaryNode {
+    type Self = Reverse
     def child = value
     override def nodeChildNames = Seq("value")
     protected[this] def nodeRebuild(child: Node) = copy(value = child) // FIXME can we factor this out together with pure? 
-    def nodeWithComputedType(scope: SymbolScope): Node = copy(this).nodeTyped(child.nodeType)
+    def nodeWithComputedType(scope: SymbolScope): Self = copy(this).nodeTyped(child.nodeType)
   }
   final case class Nullsorting(value: Node,sorting:Nullsorting.Sorting) extends UnaryNode {
+    type Self = Nullsorting
     def child = value
     override def nodeChildNames = Seq("value")
     protected[this] def nodeRebuild(child: Node) = copy(value = child) // FIXME can we factor this out together with pure? 
-    def nodeWithComputedType(scope: SymbolScope): Node = copy(this).nodeTyped(child.nodeType)
+    def nodeWithComputedType(scope: SymbolScope): Self = copy(this).nodeTyped(child.nodeType)
   }
   object Nullsorting extends Enumeration{
     type Sorting = Value
@@ -154,7 +156,7 @@ class SlickBackend( val driver: JdbcDriver, mapper:Mapper ) extends QueryableBac
         sq.CollectionTypeConstructor.default,
         sq.StructType(_fields.map( sym => columnField(sym) -> columnType(sym.typeSignature) ))
       )
-      override def nodeWithComputedType(scope: sq.SymbolScope): sq.Node = nodeRebuild
+      override def nodeWithComputedType(scope: sq.SymbolScope) = nodeRebuild
     }
     new Query( table, Scope() )
   }
