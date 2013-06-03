055a9fddcfef47e4f9062494a25a5d80ef016d07
Amir Shaikhha <ashaikhha@gmail.com>
2013-06-03 16:22:58 +0200

    test-config is now a config inside Test-kit and not a separate project.

--- project/Build.scala
+++ project/Build.scala
@@ -120,7 +120,7 @@ object SlickBuild extends Build {
       mappings in (Compile, packageBin) <++= mappings in (config("macro"), packageBin)
     ))
   lazy val slickTestkitProject = Project(id = "testkit", base = file("slick-testkit"),
-    settings = Project.defaultSettings ++ sharedSettings ++ extTarget("testkit", None) ++ Seq(
+    settings = Project.defaultSettings ++ inConfig(config("test-config"))(Defaults.configSettings) ++ sharedSettings ++ extTarget("testkit", None) ++ Seq(
       name := "Slick-TestKit",
       description := "Test Kit for Slick (Scala Language-Integrated Connection Kit)",
       scalacOptions in (Compile, doc) <++= (version).map(v => Seq("-doc-title", "Slick TestKit", "-doc-version", v)),
@@ -141,6 +141,11 @@ object SlickBuild extends Build {
         "mysql" % "mysql-connector-java" % "5.1.13" % "test",
         "net.sourceforge.jtds" % "jtds" % "1.2.4" % "test"
       ),
+      ivyConfigurations += config("test-config").hide.extend(Compile),
+      unmanagedClasspath in config("test-config") <++= fullClasspath in (slickProject, Compile),
+      unmanagedClasspath in Test <++= fullClasspath in config("test-config"),
+      mappings in (Test, packageSrc) <++= mappings in (config("test-config"), packageSrc),
+      mappings in (Test, packageBin) <++= mappings in (config("test-config"), packageBin),
       // Run the Queryable tests (which need macros) on a forked JVM
       // to avoid classloader problems with reification
       testGrouping <<= definedTests in Test map partitionTests,
@@ -155,12 +160,7 @@ object SlickBuild extends Build {
     //test <<= Seq(test in Test, test in DocTest).dependOn,
     //concurrentRestrictions += Tags.limitSum(1, Tags.Test, Tags.ForkedTestGroup),
     //concurrentRestrictions in Global += Tags.limit(Tags.Test, 1),
-  ) dependsOn(slickTestkitConfigProject, slickProject)
-
-  lazy val slickTestkitConfigProject = Project(id = "testkit-config", base = file("slick-testkit/config"),
-    settings = Project.defaultSettings ++ sharedSettings ++ Seq(
-      publish := {}
-      )).dependsOn(slickProject)
+  ) dependsOn(slickProject)
 
   /* Test Configuration for running tests on doc sources */
   lazy val DocTest = config("doctest") extend(Test)
