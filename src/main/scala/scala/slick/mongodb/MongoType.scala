eeb94a5afe611e348af7bccc5748cea253aab0d6
Brendan W. McAdams <bwmcadams@evilmonkeylabs.com>
2013-08-07 10:59:41 -0700

    Building type mappings

--- src/main/scala/scala/slick/mongodb/MongoType.scala
+++ src/main/scala/scala/slick/mongodb/MongoType.scala
@@ -4,6 +4,7 @@ import scala.slick.ast._
 import scala.reflect.ClassTag
 import com.mongodb.casbah.Imports._
 import scala.slick.profile.RelationalTypesComponent
+import java.util.UUID
 
 /**
  * A MongoType object represents a Scala type that can be
@@ -29,14 +30,19 @@ trait MongoType[T] extends TypedType[T] { self =>
    * Overwrites it if it already exists...
    */
   def setValue(k: String, v: T)(doc: DBObject): Unit = {
-
+    doc.put(k, v)
   }
 
   /**
    * Set an Option parameter of the type for a given key
    * Overwrites it if it already exists...
    */
-  def setOption(k: String, v: Option[T])(doc: DBObject): Unit
+  def setOption(k: String, v: Option[T])(doc: DBObject): Unit = v match {
+    case Some(value) =>
+      doc.put(k, value)
+    case None => // TODO - should we set null? Or leave the key out? I like leaving the structure in
+      doc.put(k, null)
+  }
 
 
   /**
@@ -72,14 +78,109 @@ trait MongoTypesComponent extends RelationalTypesComponent { driver: MongoDriver
   }
 
   class MongoTypes {
-    val booleanMongoType = new BooleanMongoType
+    // TODO - for now, any types that are in my chart of "not really supported" like Char/Byte are left out.
+    /**
+     ____ Essential Types (+ supported, - not supported, ~ "questions about supportability") ____
 
-    class BooleanMongoType extends MongoType[Boolean] {
+     Boolean +
+     Char  ~ (Mongo lacks a Char type.. we might have to map on string or an int32?)
+     Int +
+     Long +
+     Null + (There's a special 'null' type in mongo though this wouldn't be a FIELD type I assume)
+     String +
+     Unit ~ (Not sure what the type mapping to mongo of Unit would be?)
+
+     _____ JDBC Types (+ supported, - not supported, ~ "questions about supportability") ____
+
+
+     Boolean +
+     Blob ~ ( Not a specific Mongo blob type ; We can use binary blocks, or strings)
+     Clob ~ ( Not a specific Mongo clob type ; We can use binary blocks, or strings)
+     Byte ~ (Mongo lacks a single byte/char type (or even short). String or int32 to map?)
+     Char ~ (Mongo lacks a single byte/char type (or even short). String or int32 to map?)
+     Date ~  (Mongo does not have a specific DATE type; we must use a Timestamp; including time w/ date)
+     Double + (Doubles + floats both map to a Mongo double which is  [8 bytes - 64-bit iee 754 floating point])
+     Float + (Doubles + floats both map to a Mongo double which is  [8 bytes - 64-bit iee 754 floating point])
+     Int +
+     Long +
+     Short - ( There is no specific short type for mongo….)
+     String +
+     Time - (Mongo lacks a "time only component" type and I see no easy way to fudge it)
+     Timestamp +
+     Unit ~ (Not sure what the type mapping to mongo of Unit would be?)
+     UUID +
+     BigDecimal - (this is a tricky one, mongo really doesn't support BigDecimal safely. I'll talk to Rose Toomey [salat] and see how she is solving it these days) - not supported by relational profile ... Throw unsupported exception
+    */
+
+    val booleanMongoType = new BooleanMongoType
+    val byteArrayMongoType = new ByteArrayMongoType
+    val dateMongoType = new DateMongoType
+    // TODO - Add a time type that stores as a mongo date w/ just the time since Jan 1, 1970?
+    val doubleMongoType = new DoubleMongoType
+    val floatMongoType = new FloatMongoType /** Mongo only technically supports Doubles - 64 bit ieee-754 floating point */
+    val intMongoType = new IntMongoType
+    val longMongoType = new LongMongoType
+    val stringMongoType = new StringMongoType
+    val timestampMongoType = new TimestampMongoType
+    val uuidMongoType = new UUIDMongoType
+    val nullMongoType = new NullMongoType
+
+    class BooleanMongoType extends DriverMongoType[Boolean] {
       def mongoType: Byte = BSON.BOOLEAN
       def mongoTypeName: String = "Boolean"
-      def scalaType = ScalaBaseType[Boolean]
     }
 
+    class ByteArrayMongoType extends DriverMongoType[Array[Byte]] {
+      def mongoType: Byte = BSON.BINARY
+      def mongoTypeName: String = "Binary"
+    }
+
+    class DateMongoType extends DriverMongoType[java.sql.Date] {
+      def mongoType: Byte = BSON.DATE
+      def mongoTypeName: String = "Date"
+    }
+
+    class DoubleMongoType extends DriverMongoType[Double] {
+      def mongoType: Byte = BSON.NUMBER
+      def mongoTypeName: String = "Number (64-bit IEEE-754 FP aka 'Double')"
+    }
+
+    class FloatMongoType extends DriverMongoType[Double] {
+      def mongoType: Byte = BSON.NUMBER
+      def mongoTypeName: String = "Number (64-bit IEEE-754 FP aka 'Double' [as Float])"
+    }
+
+    class IntMongoType extends DriverMongoType[Int] {
+      def mongoType: Byte = BSON.NUMBER_INT
+      def mongoTypeName: String = "Int"
+    }
+
+    class LongMongoType extends DriverMongoType[Long] {
+      def mongoType: Byte = BSON.NUMBER_LONG
+      def mongoTypeName: String = "Long"
+    }
+
+    class StringMongoType extends DriverMongoType[String] {
+      def mongoType: Byte = BSON.STRING
+      def mongoTypeName: String = "String"
+    }
+
+    // TODO - I don't think we can properly store the nanoseconds permitted in java.sql.Timestamp...
+    class TimestampMongoType extends DriverMongoType[java.sql.Timestamp] {
+      def mongoType: Byte = BSON.DATE
+      def mongoTypeName: String = "Date [as Timestamp]"
+    }
+
+    class UUIDMongoType extends DriverMongoType[UUID] {
+      // TODO - We need binary subtyping.. ugh.
+      def mongoType: Byte = BSON.B_UUID
+      def mongoTypeName: String = "UUID"
+    }
+
+    class NullMongoType extends DriverMongoType[Null] {
+      def mongoType: Byte = BSON.NULL
+      def mongoTypeName: String = "Null"
+    }
   }
 
 }
