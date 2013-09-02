slick/slick

commit 83c487ded5e79804a4f4e45e9214e6e4e04bac8d

Improve handling of types in the AST and query compiler.
- Add an option to fill in missing types without retyping everything in
  Node.nodeWithComputedChildren.

- Add nominal types for Tables and Pure nodes. All types get a structural
  expansion. The identity of these types is defined by a TypeSymbol. The
  inferTypes phase computes the correct table types with all required
  columns.

- Define utility functions such as mapChildren() and select() directly in
  the Type trait.

- Unify methods for transformations with and without keeping type
  information.

- Add -Dscala.slick.ansiDump=true to enable ANSI colors in AST dumps to
  make them more readable with all the verbose type information.

- Add -Dscala.slick.dumpPaths=true to show children of paths in AST dumps
  (so you can see their type information).

src/main/scala/scala/slick/ast/Dump.scala
@@ -3,11 +3,18 @@ package scala.slick.ast
 import java.io.{OutputStreamWriter, StringWriter, PrintWriter}
 import scala.collection.mutable.HashSet
 import Util.nodeToNodeOps
+import scala.sys.BooleanProp
 
 /**
  * Create a readable printout of an AST
  */
 object Dump {
+  private[ast] val (normal, green, yellow, blue) = {
+    if(BooleanProp.valueIsTrue("scala.slick.ansiDump")) ("\u001B[0m", "\u001B[32m", "\u001B[33m", "\u001B[34m")
+    else ("", "", "", "")
+  }
+  private[ast] val dumpPaths: Boolean = BooleanProp.valueIsTrue("scala.slick.dumpPaths")
+
   def get(n: NodeGenerator, name: String = "", prefix: String = "") = {
     val buf = new StringWriter
     apply(n, name, prefix, new PrintWriter(buf))
@@ -17,8 +24,8 @@ object Dump {
   def apply(n: NodeGenerator, name: String = "", prefix: String = "", to: PrintWriter = null, typed: Boolean = true) {
     val out = if(to eq null) new PrintWriter(new OutputStreamWriter(System.out)) else to
     val dc = new DumpContext(out, typed)
-    dc.dump(Node(n), prefix, name)
-    for(i <- dc.orphans) dc.dump(i.target, prefix, "/"+i.name+": ")
+    dc.dump(Node(n), prefix, name, true)
+    for(i <- dc.orphans) dc.dump(i.target, prefix, "/"+i.name+": ", true)
     out.flush()
   }
 }
@@ -52,26 +59,25 @@ class DumpContext(val out: PrintWriter, val typed: Boolean = true) {
     (refs -- defs).toSet
   }
 
-  def dump(tree: Node, prefix: String, name: String) {
+  def dump(tree: Node, prefix: String, name: String, topLevel: Boolean) {
+    import Dump._
     tree match {
       case n: DefNode => n.nodeGenerators.foreach(t => addDef(t._1))
       case RefNode(s) => addRef(s)
       case _ =>
     }
-    val tpe = tree match {
-      case t: Typed => t.tpe
-      case t => t.nodeType
-    }
-    val typeInfo = if(typed && tpe != UnassignedType) " : " + tpe.toString else ""
+    val tpe = tree.nodeType
+    val typeInfo = if(typed && tpe != UnassignedType) blue + " : " + tpe.toString + normal else ""
+    val start = yellow + prefix + name + normal
+    def tl(s: Any) = if(topLevel) green + s + normal else s
+    out.println(start + tl(tree) + typeInfo)
     tree match {
-      case Path(l @ (_ :: _ :: _)) =>
-        // Print paths on a single line
-        out.println(prefix + name + Path.toString(l) + typeInfo)
+      case Path(l @ (_ :: _ :: _)) if !dumpPaths =>
+        // Omit path details unless dumpPaths is set
         tree.foreach { case RefNode(s) => addRef(s) }
       case _ =>
-        out.println(prefix + name + tree + typeInfo)
         for((chg, n) <- tree.nodeChildren.zip(tree.nodeChildNames))
-          dump(chg, prefix + "  ", n+": ")
+          dump(chg, prefix + "  ", n+": ", false)
     }
   }
 }

slick-testkit/src/test/resources/logback-test.xml
@@ -10,9 +10,9 @@
     <logger name="scala.slick.compiler"                           level="${log.qcomp:-info}" />
     <logger name="scala.slick.compiler.QueryCompiler"             level="${log.qcomp.phases:-inherited}" />
     <logger name="scala.slick.compiler.Inline"                    level="${log.qcomp.inline:-inherited}" />
-    <logger name="scala.slick.compiler.RewriteOrderBy"            level="${log.qcomp.rewriteOrderBy:-inherited}" />
     <logger name="scala.slick.compiler.AssignUniqueSymbols"       level="${log.qcomp.assignUniqueSymbols:-inherited}" />
     <logger name="scala.slick.compiler.ExpandTables"              level="${log.qcomp.expandTables:-inherited}" />
+    <logger name="scala.slick.compiler.InferTypes"                level="${log.qcomp.inferTypes:-inherited}" />
     <logger name="scala.slick.compiler.CreateResultSetMapping"    level="${log.qcomp.createResultSetMapping:-inherited}" />
     <logger name="scala.slick.compiler.ForceOuterBinds"           level="${log.qcomp.forceOuterBinds:-inherited}" />
     <logger name="scala.slick.compiler.ExpandRefs"                level="${log.qcomp.expandRefs:-inherited}" />

src/main/scala/scala/slick/ast/ClientSideOp.scala
@@ -29,16 +29,15 @@ object ClientSideOp {
 final case class First(val child: Node) extends UnaryNode with ClientSideOp {
   type Self = First
   protected[this] def nodeRebuild(ch: Node) = copy(child = ch)
-  def nodeWithComputedType(scope: SymbolScope, retype: Boolean): Self = if(nodeHasType && !retype) this else {
-    val this2 = nodeMapChildren(_.nodeWithComputedType(scope, retype))
-    val tp = this2.nodeChildren.head.nodeType.asCollectionType.elementType
-    nodeBuildTypedNode(this2, tp)
-  }
-  def nodeMapServerSide(keepType: Boolean, r: Node => Node) = {
-    val g = (ch: Node) => r(ch)
-    if(keepType) nodeMapChildrenKeepType(g)
-    else nodeMapChildren(g)
-  }
+  def nodeWithComputedType(scope: SymbolScope, typeChildren: Boolean, retype: Boolean): Self =
+    if(nodeHasType && !typeChildren) this else {
+      val this2 = nodeMapChildren(_.nodeWithComputedType(scope, typeChildren, retype), !retype)
+      if(!nodeHasType || retype) {
+        val tp = this2.nodeChildren.head.nodeType.asCollectionType.elementType
+        nodeBuildTypedNode(this2, tp)
+      } else this2
+    }
+  def nodeMapServerSide(keepType: Boolean, r: Node => Node) = nodeMapChildren(r, keepType)
 }
 
 /** A client-side projection of type
@@ -56,19 +55,25 @@ final case class ResultSetMapping(generator: Symbol, from: Node, map: Node) exte
   def nodeGenerators = Seq((generator, from))
   override def toString = "ResultSetMapping"
   protected[this] def nodeRebuildWithGenerators(gen: IndexedSeq[Symbol]) = copy(generator = gen(0))
-  def nodeWithComputedType(scope: SymbolScope, retype: Boolean): Self = if(nodeHasType && !retype) this else {
-    val f2 = from.nodeWithComputedType(scope, retype)
-    val (s2, newType) = f2.nodeType match {
-      case CollectionType(cons, elem) =>
-        val s2 = map.nodeWithComputedType(scope + (generator -> elem), retype)
-        (s2, CollectionType(cons, s2.nodeType))
-      case t =>
-        val s2 = map.nodeWithComputedType(scope + (generator -> t), retype)
-        (s2, s2.nodeType)
+  def nodeWithComputedType(scope: SymbolScope, typeChildren: Boolean, retype: Boolean): Self =
+    if(nodeHasType && !typeChildren) this else {
+      val f2 = from.nodeWithComputedType(scope, typeChildren, retype)
+      val (s2, newType) = f2.nodeType match {
+        case CollectionType(cons, elem) =>
+          val s2 = map.nodeWithComputedType(scope + (generator -> elem), typeChildren, retype)
+          (s2, CollectionType(cons, s2.nodeType))
+        case t =>
+          val s2 = map.nodeWithComputedType(scope + (generator -> t), typeChildren, retype)
+          (s2, s2.nodeType)
+      }
+      if(!nodeHasType || retype) {
+        if((f2 eq from) && (s2 eq map) && newType == nodeType) this
+        else copy(from = f2, map = s2).nodeTyped(newType)
+      } else {
+        if((f2 eq from) && (s2 eq map)) this
+        else copy(from = f2, map = s2).nodeTyped(nodeType)
+      }
     }
-    if((f2 eq from) && (s2 eq map) && newType == nodeType) this
-    else copy(from = f2, map = s2).nodeTyped(newType)
-  }
   def nodeMapServerSide(keepType: Boolean, r: Node => Node) = {
     val this2 = nodeMapScopedChildren {
       case (Some(_), ch) => r(ch)

src/main/scala/scala/slick/ast/Comprehension.scala
@@ -35,41 +35,45 @@ final case class Comprehension(from: Seq[(Symbol, Node)] = Seq.empty, where: Seq
   override def toString = "Comprehension(fetch = "+fetch+", offset = "+offset+")"
   protected[this] def nodeRebuildWithGenerators(gen: IndexedSeq[Symbol]) =
     copy(from = (from, gen).zipped.map { case ((_, n), s) => (s, n) })
-  def nodeWithComputedType(scope: SymbolScope, retype: Boolean): Self = if(nodeHasType && !retype) this else {
-    // Assign types to all "from" Nodes and compute the resulting scope
-    val (genScope, f2) = from.foldLeft((scope, Vector.empty[Node])) { case ((sc, n2s), (s, n)) =>
-      val n2 = n.nodeWithComputedType(sc, retype)
-      val sc2 = sc + (s -> n2.nodeType.asCollectionType.elementType)
-      (sc2, n2s :+ n2)
+  def nodeWithComputedType(scope: SymbolScope, typeChildren: Boolean, retype: Boolean): Self =
+    if(nodeHasType && !typeChildren) this else {
+      // Assign types to all "from" Nodes and compute the resulting scope
+      val (genScope, f2) = from.foldLeft((scope, Vector.empty[Node])) { case ((sc, n2s), (s, n)) =>
+        val n2 = n.nodeWithComputedType(sc, typeChildren, retype)
+        val sc2 = sc + (s -> n2.nodeType.asCollectionType.elementType)
+        (sc2, n2s :+ n2)
+      }
+      // Assign types to "where", "groupBy", "orderBy" and "select" Nodes
+      val w2 = mapOrNone(where)(_.nodeWithComputedType(genScope, typeChildren, retype))
+      val g2 = mapOrNone(groupBy)(_.nodeWithComputedType(genScope, typeChildren, retype))
+      val o = orderBy.map(_._1)
+      val o2 = mapOrNone(o)(_.nodeWithComputedType(genScope, typeChildren, retype))
+      val s2 = mapOrNone(select)(_.nodeWithComputedType(genScope, typeChildren, retype))
+      // Check if the nodes changed
+      val same = (from, f2).zipped.map(_._2 eq _).forall(identity) &&
+        w2.isEmpty && g2.isEmpty && o2.isEmpty && s2.isEmpty
+      val newSel = s2.map(_.headOption).getOrElse(select)
+      val newType =
+        if(!nodeHasType || retype) {
+          newSel match {
+            case Some(sel) => sel.nodeType
+            case None =>
+              val el = f2.last.nodeType.asCollectionType.elementType
+              val tc = f2.head.nodeType.asCollectionType.cons
+              CollectionType(tc, el)
+          }
+        } else nodeType
+      if(same && newType == nodeType) this else {
+        // Compute result type
+        copy(
+          from = (from, f2).zipped.map { case ((s, _), n) => (s, n) },
+          where = w2.getOrElse(where),
+          groupBy = g2.map(_.headOption).getOrElse(groupBy),
+          orderBy = o2.map(o2 => (orderBy, o2).zipped.map { case ((_, o), n) => (n, o) }).getOrElse(orderBy),
+          select = s2.map(_.headOption).getOrElse(select)
+        ).nodeTyped(newType)
+      }
     }
-    // Assign types to "where", "groupBy", "orderBy" and "select" Nodes
-    val w2 = mapOrNone(where)(_.nodeWithComputedType(genScope, retype))
-    val g2 = mapOrNone(groupBy)(_.nodeWithComputedType(genScope, retype))
-    val o = orderBy.map(_._1)
-    val o2 = mapOrNone(o)(_.nodeWithComputedType(genScope, retype))
-    val s2 = mapOrNone(select)(_.nodeWithComputedType(genScope, retype))
-    // Check if the nodes changed
-    val same = (from, f2).zipped.map(_._2 eq _).forall(identity) &&
-      w2.isEmpty && g2.isEmpty && o2.isEmpty && s2.isEmpty
-    val newSel = s2.map(_.headOption).getOrElse(select)
-    val newType = (newSel match {
-      case Some(sel) => sel.nodeType
-      case None =>
-        val el = f2.last.nodeType.asCollectionType.elementType
-        val tc = f2.head.nodeType.asCollectionType.cons
-        CollectionType(tc, el)
-    })
-    if(same && newType == nodeType) this else {
-      // Compute result type
-      copy(
-        from = (from, f2).zipped.map { case ((s, _), n) => (s, n) },
-        where = w2.getOrElse(where),
-        groupBy = g2.map(_.headOption).getOrElse(groupBy),
-        orderBy = o2.map(o2 => (orderBy, o2).zipped.map { case ((_, o), n) => (n, o) }).getOrElse(orderBy),
-        select = s2.map(_.headOption).getOrElse(select)
-      ).nodeTyped(newType)
-    }
-  }
 }
 
 /** The row_number window function */

src/main/scala/scala/slick/ast/Insert.scala
@@ -10,11 +10,17 @@ final case class Insert(generator: Symbol, table: Node, map: Node, linear: Node)
   def nodeGenerators = Vector((generator, table))
   def nodeRebuild(ch: IndexedSeq[Node]) = copy(table = ch(0), map = ch(1), linear = ch(2))
   def nodeRebuildWithGenerators(gen: IndexedSeq[Symbol]) = copy(generator = gen(0))
-  def nodeWithComputedType(scope: SymbolScope, retype: Boolean): Self = if(nodeHasType && !retype) this else {
-    val t2 = table.nodeWithComputedType(scope, retype)
-    val m2 = map.nodeWithComputedType(scope + (generator -> t2.nodeType), retype)
-    val newType = m2.nodeType
-    if((t2 eq table) && (m2 eq map) && newType == nodeType) this
-    else copy(table = t2, map = m2).nodeTyped(newType)
-  }
+  def nodeWithComputedType(scope: SymbolScope, typeChildren: Boolean, retype: Boolean): Self =
+    if(nodeHasType && !typeChildren) this else {
+      val t2 = table.nodeWithComputedType(scope, typeChildren, retype)
+      val m2 = map.nodeWithComputedType(scope + (generator -> t2.nodeType), typeChildren, retype)
+      if(!nodeHasType || retype) {
+        val newType = m2.nodeType
+        if((t2 eq table) && (m2 eq map) && newType == nodeType) this
+        else copy(table = t2, map = m2).nodeTyped(newType)
+      } else {
+        if((t2 eq table) && (m2 eq map)) this
+        else copy(table = t2, map = m2).nodeTyped(nodeType)
+      }
+    }
 }

src/main/scala/scala/slick/ast/Symbol.scala
@@ -18,6 +18,25 @@ case class ElementSymbol(idx: Int) extends Symbol {
   def name = "_" + idx
 }
 
+/** The symbol of a nominal type */
+trait TypeSymbol extends Symbol
+
+/** A TypeSymbol which uniquely identifies a table type */
+trait TableIdentitySymbol extends TypeSymbol
+
+/** Default implementation of TableIdentitySymbol */
+case class SimpleTableIdentitySymbol(constituents: AnyRef*) extends TableIdentitySymbol {
+  def name = constituents.mkString("@(", ".", ")")
+}
+
+/** An anonymous symbol defined in the AST. */
+class AnonTypeSymbol extends TypeSymbol {
+  def name = "$@"+System.identityHashCode(this)
+}
+
+object AnonTypeSymbol {
+  def unapply(a: AnonTypeSymbol) = Some(a.name)
+}
 
 /** An anonymous symbol defined in the AST. */
 class AnonSymbol extends Symbol {

src/main/scala/scala/slick/ast/Type.scala
@@ -4,23 +4,36 @@ import scala.language.implicitConversions
 import scala.slick.SlickException
 import scala.collection.generic.CanBuildFrom
 import scala.reflect.ClassTag
+import Util._
 
 /** Super-trait for all types */
-trait Type
-
-object Type {
-  def select(tpe: Type, sym: Symbol): Type = (tpe, sym) match {
-    case (StructType(es), _) => es.find(x => x._1 == sym).map(_._2).
-      getOrElse(throw new SlickException("No type for symbol "+sym+" found in "+tpe))
-    case (ProductType(es), ElementSymbol(i)) if i <= es.length => es(i-1)
-    case _ => throw new SlickException("No type for symbol "+sym+" found in "+tpe)
-  }
+trait Type {
+  /** Apply a transformation to all type children and reconstruct this
+    * type with the new children, or return the original object if no
+    * child is changed. */
+  def mapChildren(f: Type => Type): Type
+  def select(sym: Symbol): Type =
+    throw new SlickException("No type for symbol "+sym+" found in "+this)
+  /** The structural view of this type */
+  def structural: Type = this
 }
 
-case class StructType(elements: Seq[(Symbol, Type)]) extends Type {
+/** An atomic type (i.e. a type which does not contain other types) */
+trait AtomicType extends Type {
+  final def mapChildren(f: Type => Type): this.type = this
+}
+
+final case class StructType(elements: Seq[(Symbol, Type)]) extends Type {
   override def toString = "{" + elements.iterator.map{ case (s, t) => s + ": " + t }.mkString(", ") + "}"
   lazy val symbolToIndex: Map[Symbol, Int] =
     elements.zipWithIndex.map { case ((sym, _), idx) => (sym, idx) }(collection.breakOut)
+  def mapChildren(f: Type => Type): StructType =
+    mapOrNone(elements.map(_._2))(f) match {
+      case Some(types2) => StructType((elements, types2).zipped.map((e, t) => (e._1, t)))
+      case None => this
+    }
+  override def select(sym: Symbol) =
+    elements.find(x => x._1 == sym).map(_._2).getOrElse(super.select(sym))
 }
 
 trait OptionType extends Type {
@@ -31,15 +44,34 @@ trait OptionType extends Type {
 object OptionType {
   def apply(tpe: Type): OptionType = new OptionType {
     def elementType = tpe
+    def mapChildren(f: Type => Type): OptionType = {
+      val e2 = f(elementType)
+      if(e2 eq elementType) this
+      else OptionType(e2)
+    }
   }
 }
 
-case class ProductType(elements: IndexedSeq[Type]) extends Type {
+final case class ProductType(elements: IndexedSeq[Type]) extends Type {
   override def toString = "(" + elements.mkString(", ") + ")"
+  def mapChildren(f: Type => Type): ProductType =
+    mapOrNone(elements)(f) match {
+      case Some(e2) => ProductType(e2)
+      case None => this
+    }
+  override def select(sym: Symbol) = sym match {
+    case ElementSymbol(i) if i <= elements.length => elements(i-1)
+    case _ => super.select(sym)
+  }
 }
 
-case class CollectionType(cons: CollectionTypeConstructor, elementType: Type) extends Type {
+final case class CollectionType(cons: CollectionTypeConstructor, elementType: Type) extends Type {
   override def toString = cons + "[" + elementType + "]"
+  def mapChildren(f: Type => Type): CollectionType = {
+    val e2 = f(elementType)
+    if(e2 eq elementType) this
+    else CollectionType(cons, e2)
+  }
 }
 
 case class CollectionTypeConstructor(dummy: String = "") {
@@ -51,16 +83,40 @@ object CollectionTypeConstructor {
   def default = new CollectionTypeConstructor
 }
 
-trait MappedScalaType extends Type {
-  def baseType: Type
-  def toMapped(v: Any): Any
-  def toBase(v: Any): Any
+final class MappedScalaType(val baseType: Type, _toBase: Any => Any, _toMapped: Any => Any) extends Type {
+  def toBase(v: Any): Any = _toBase(v)
+  def toMapped(v: Any): Any = _toMapped(v)
   override def toString = s"Mapped[$baseType]"
+  def mapChildren(f: Type => Type): MappedScalaType = {
+    val e2 = f(baseType)
+    if(e2 eq baseType) this
+    else new MappedScalaType(e2, _toBase, _toMapped)
+  }
 }
 
-case object NoType extends Type
+final case object NoType extends AtomicType
 
-case object UnassignedType extends Type
+final case object UnassignedType extends AtomicType
+
+/* A type with a name, as used by tables.
+ *
+ * Compiler phases which change types may keep their own representation
+ * of the structural view but must update the AST at the end of the phase
+ * so that all NominalTypes with the same symbol have the same structural
+ * view. */
+final case class NominalType(sym: TypeSymbol)(val structuralView: Type) extends Type {
+  def toShortString = s"NominalType($sym)"
+  override def toString = s"$toShortString($structuralView)"
+  def withStructuralView(t: Type): NominalType =
+    if(t == structuralView) this else copy()(t)
+  override def structural: Type = structuralView.structural
+  override def select(sym: Symbol): Type = structuralView.select(sym)
+  def mapChildren(f: Type => Type): NominalType = {
+    val struct2 = f(structuralView)
+    if(struct2 eq structuralView) this
+    else new NominalType(sym)(struct2)
+  }
+}
 
 /** Something that has a type */
 trait Typed {
@@ -76,11 +132,16 @@ trait TypedType[T] extends Type { self =>
   def optionType: OptionTypedType[T] = new OptionTypedType[T] {
     val elementType = self
     def scalaType = new ScalaOptionType[T](self.scalaType)
+    def mapChildren(f: Type => Type): OptionTypedType[T] = {
+      val e2 = f(elementType)
+      if(e2 eq elementType) this
+      else e2.asInstanceOf[TypedType[T]].optionType
+    }
   }
   def scalaType: ScalaType[T]
 }
 
-trait BaseTypedType[T] extends TypedType[T]
+trait BaseTypedType[T] extends TypedType[T] with AtomicType
 
 trait OptionTypedType[T] extends TypedType[Option[T]] with OptionType {
   val elementType: TypedType[T]
@@ -102,10 +163,23 @@ class TypeUtil(val tpe: Type) extends AnyVal {
     case o: OptionType => o
     case _ => throw new SlickException("Expected an option type, found "+tpe)
   }
+  def replace(f: PartialFunction[Type, Type]): Type = TypeUtilOps.replace(tpe, f)
 }
 
 object TypeUtil {
   implicit def typeToTypeUtil(tpe: Type) = new TypeUtil(tpe)
+
+  /* An extractor for node types */
+  object :@ {
+    def unapply(n: Node) = Some((n, n.nodeType))
+  }
+}
+
+object TypeUtilOps {
+  import TypeUtil.typeToTypeUtil
+
+  def replace(tpe: Type, f: PartialFunction[Type, Type]): Type =
+    f.applyOrElse(tpe, ({ case t: Type => t.mapChildren(_.replace(f)) }): PartialFunction[Type, Type])
 }
 
 trait SymbolScope {
@@ -224,5 +298,10 @@ class ScalaOptionType[T](val elementType: ScalaType[T]) extends ScalaType[Option
         else base.compare(x.get, y.get)
       }
     }
+  }
+  def mapChildren(f: Type => Type): ScalaOptionType[T] = {
+    val e2 = f(elementType)
+    if(e2 eq elementType) this
+    else e2.asInstanceOf[ScalaType[T]].optionType
   }
 }

commit 56cdd2da7df9b6fd6fbeb17a3d193f5278154f89

Fix MatchError in tree dumps.
The bug was introduced in 83c487ded5e79804a4f4e45e9214e6e4e04bac8d.

src/main/scala/scala/slick/ast/Dump.scala
@@ -74,7 +74,10 @@ class DumpContext(val out: PrintWriter, val typed: Boolean = true) {
     tree match {
       // Omit path details unless dumpPaths is set
       case Path(l @ (_ :: _ :: _)) if !dumpPaths =>
-        tree.foreach { case Ref(s) => addRef(s) }
+        tree.foreach {
+          case Ref(s) => addRef(s)
+          case _ =>
+        }
       case _ =>
         for((chg, n) <- tree.nodeChildren.zip(tree.nodeChildNames))
           dump(chg, prefix + "  ", n+": ", false)