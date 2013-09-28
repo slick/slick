f52b0042666e7cd9578e727e25695414b7ca5d86
prakhunov <prakhunov@gmail.com>
2013-09-28 00:03:04 -0500

    refactored ColumnDefaultTest

--- slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/ColumnDefaultTest.scala
+++ slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/ColumnDefaultTest.scala
@@ -6,33 +6,33 @@ import com.typesafe.slick.testkit.util.{RelationalTestDB, TestkitTest}
 class ColumnDefaultTest extends TestkitTest[RelationalTestDB] {
   import tdb.profile.simple._
 
-  class A(tag: Tag) extends Table[(Int, String, Option[Boolean])](tag, "a") {
+  abstract class A[T](tag: Tag, tableName: String) extends Table[T](tag, tableName) {
     def id = column[Int]("id")
-    def a = column[String]("a", O.Default("foo"))
-    def b = column[Option[Boolean]]("b", O.Default(Option(true)))
-    def * = (id, a, b)
+    def a = column[String]("a", O Default "foo")
+    def b = column[Option[Boolean]]("b", O Default Option(true))
   }
-  lazy val as = TableQuery[A]
 
-  class B(tag: Tag) extends Table[(Int, String, Option[Boolean], String)](tag, "b") {
-    def id = column[Int]("id")
-    def a = column[String]("a", O.Default("foo"))
-    def b = column[Boolean]("b", O.Default(true))
-    def c = column[String]("c", O.Default(SimpleFunction.unary[String, String]("UPPER").apply("test")))
-    def * = (id, a, b.?, c)
+  class B(tag: Tag) extends A[(Int, String, Option[Boolean])](tag, "b") {
+    def * = (id, a, b)
   }
   lazy val bs = TableQuery[B]
 
+  class C(tag: Tag) extends A[(Int, String, Option[Boolean], String)](tag, "c") {
+    def c = column[String]("c", O Default SimpleFunction.unary[String, String]("UPPER").apply("test"))
+    def * = (id, a, b, c)
+  }
+  lazy val cs = TableQuery[C]
+
   def test = ifCap(rcap.columnDefaults) {
-    as.ddl.create
-    as.map(_.id) += 42
-    assertEquals(List((42, "foo", Some(true))), as.run)
-      
+    bs.ddl.create
+    bs.map(_.id) += 42
+    assertEquals(List((42, "foo", Some(true))), bs.run)
+
     ifCap(jcap.columnFunctionDefaults) {
-		  bs.ddl.create
-	    bs.map(_.id) += 42
-      assertEquals(List((42, "foo", Some(true), "TEST")), bs.run)
-	  }
+      cs.ddl.create
+      cs.map(_.id) += 42
+      assertEquals(List((42, "foo", Some(true), "TEST")), cs.run)
+    }
   }
 }
 
