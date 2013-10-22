cba23921a045abbd71271435f2e72bddf02b9cda
Erik Post <erik@shinsetsu.nl>
2013-10-22 17:21:55 +0200

    Quote table and fk names in MySQL ALTER TABLE statements
    Slick generates unquoted ALTER TABLE statements for MySQL, for example:
    
        ALTER TABLE group DROP FOREIGN KEY foo_fk;
    
    This will fail with a MySQL syntax error. This fixes things so that we get quoted identifiers:
    
        ALTER TABLE `group` DROP FOREIGN KEY `foo_fk`;

--- src/main/scala/scala/slick/driver/MySQLDriver.scala
+++ src/main/scala/scala/slick/driver/MySQLDriver.scala
@@ -113,7 +113,7 @@ trait MySQLDriver extends JdbcDriver { driver =>
 
   class TableDDLBuilder(table: Table[_]) extends super.TableDDLBuilder(table) {
     override protected def dropForeignKey(fk: ForeignKey) = {
-      "ALTER TABLE " + table.tableName + " DROP FOREIGN KEY " + fk.name
+      "ALTER TABLE " + quoteIdentifier(table.tableName) + " DROP FOREIGN KEY " + quoteIdentifier(fk.name)
     }
   }
 
