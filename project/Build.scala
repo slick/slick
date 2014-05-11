407e8c53e92d852f72936e777f7dbb522d30694f
Christopher Vogt <christopher.vogt@epfl.ch>
2014-05-10 20:46:16 -0400

    Release 2.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -51,7 +51,7 @@ object SlickBuild extends Build {
   }
 
   lazy val sharedSettings = Seq(
-    version := "2.0.2-RC1",
+    version := "2.0.2",
     organizationName := "Typesafe",
     organization := "com.typesafe.slick",
     resolvers += Resolver.sonatypeRepo("snapshots"),
