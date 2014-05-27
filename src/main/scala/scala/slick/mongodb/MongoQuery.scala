d9a667d2852141cd162bfa0850e441e9b09bb1de
dvinokurov <dvinokurov777@gmail.com>
2014-05-28 01:00:16 +0300

    made MongoQuery work - commented unfinished functionality extending from Traversable

--- src/main/scala/scala/slick/mongodb/MongoQuery.scala
+++ src/main/scala/scala/slick/mongodb/MongoQuery.scala
@@ -5,18 +5,19 @@ import com.mongodb.casbah.Implicits._
 import scala.util.parsing.json.JSON
 
 // TODO: check if we really need pconv, rconv and invoker here
-class MongoQuery [-P,+R](collection:String,query: Option[String], rconv: GetResult[R]) extends Traversable[R] {
+//class MongoQuery [-P,+R](collection:String,query: Option[String], rconv: GetResult[R]) extends Traversable[R] {
+class MongoQuery [-P,+R](collection:String,query: Option[String], rconv: GetResult[R])  {
 
   //TODO: do we need foreach manually implemented here? - jdbc's static query doesn't have it
   def foreach[U](f: MongoDBObject => U)(implicit session: MongoBackend#Session) = {
     // cannot pass f to foreach because of implicit conversion from DBObject to MongoDBObject:
     session.find(collection).foreach(dbObject => f(dbObject))
   }
-
-  override def foreach[U](f: (R) => U): Unit = {
-    val g: MongoDBObject => R = mongoDBObject => JSON.parseFull(mongoDBObject)
-    foreach(g.andThen(f))
-  }
+//
+//  override def foreach[U](f: (R) => U): Unit = {
+//    val g: MongoDBObject => R = ??? // mongoDBObject => JSON.parseFull(mongoDBObject)
+//    foreach(g.andThen(f))
+//  }
 }
 // TODO: think how collection name may be received implicitly from result type
 // Example: instead of
