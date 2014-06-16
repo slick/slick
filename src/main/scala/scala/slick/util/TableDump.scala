885ff9f6251048dacdec6096edd457c98495a4eb
Stefan Zeiger <szeiger@novocode.com>
2014-06-16 18:41:42 +0200

    Fix result set debug logging, broken by the previous commit

--- src/main/scala/scala/slick/util/TableDump.scala
+++ src/main/scala/scala/slick/util/TableDump.scala
@@ -8,25 +8,27 @@ class TableDump(maxColumnWidth: Int = 20) {
   protected[this] val spaces = Iterator.fill(maxColumnWidth+2)(' ').mkString
 
   protected[this] def formatLine(line: IndexedSeq[Any]): IndexedSeq[String] = line.map { v =>
-    val s = if(v == null) "NULL" else v.toString
-    val s2 = s.replace("\r", "\\r").replace("\n", "\\n").replace("\t", "\\t")
-    if(s2.length > maxColumnWidth) s2.substring(0, maxColumnWidth-3) + TreeDump.green+"..."+TreeDump.normal else s2
+    (if(v == null) "NULL" else v.toString).replace("\r", "\\r").replace("\n", "\\n").replace("\t", "\\t")
   }
 
   def apply(header: IndexedSeq[String], data: IndexedSeq[IndexedSeq[Any]]): IndexedSeq[String] = {
     val columns = header.length
     val texts = formatLine(header) +: data.map(formatLine)
-    val widths = 0.until(columns).map(idx => texts.map(_.apply(idx).length).max)
+    val widths = 0.until(columns).map { idx => math.min(maxColumnWidth, texts.map(_.apply(idx).length).max) }
     val buf = new ArrayBuffer[String](data.length + 4)
     buf += TreeDump.blue + widths.map(l => dashes.substring(0, l+2)).mkString("/", "+", "\\") + TreeDump.normal
     var first = true
+    def pad(s: String, len: Int): String = {
+      if(s.length > maxColumnWidth) s.substring(0, maxColumnWidth-3) + TreeDump.cyan+"..."
+      else s + spaces.substring(0, len-s.length)
+    }
     for(line <- texts) {
       if(first) {
-        buf += (line, widths).zipped.map((s, len) => TreeDump.yellow+" "+s+spaces.substring(0, len-s.length)+" ").mkString(TreeDump.blue+"|", TreeDump.blue+"|", TreeDump.blue+"|"+TreeDump.normal)
+        buf += (line, widths).zipped.map((s, len) => TreeDump.yellow+" "+pad(s, len)+" ").mkString(TreeDump.blue+"|", TreeDump.blue+"|", TreeDump.blue+"|"+TreeDump.normal)
         buf += TreeDump.blue + widths.map(l => dashes.substring(0, l+2)).mkString("+", "+", "+") + TreeDump.normal
         first = false
       } else {
-        buf += (line, widths).zipped.map((s, len) => TreeDump.normal+" "+s+spaces.substring(0, len-s.length)+" ").mkString(TreeDump.blue+"|", TreeDump.blue+"|", TreeDump.blue+"|"+TreeDump.normal)
+        buf += (line, widths).zipped.map((s, len) => TreeDump.normal+" "+pad(s, len)+" ").mkString(TreeDump.blue+"|", TreeDump.blue+"|", TreeDump.blue+"|"+TreeDump.normal)
       }
     }
     buf += TreeDump.blue + widths.map(l => dashes.substring(0, l+2)).mkString("\\", "+", "/") + TreeDump.normal
