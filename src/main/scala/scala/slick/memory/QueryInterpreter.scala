52d755ec368be865efbcf85942ddc749187e28da
Christopher Vogt <christopher.vogt@epfl.ch>
2014-06-17 17:20:25 +0200

    Fix substring and push string operation up to RelationalDriver
    The substring bug was that the start and end positions were passed to the sql function, but that takes start position and length instead.

--- src/main/scala/scala/slick/memory/QueryInterpreter.scala
+++ src/main/scala/scala/slick/memory/QueryInterpreter.scala
@@ -389,6 +389,14 @@ class QueryInterpreter(db: HeapBackend#Database, params: Any) extends Logging {
     case Library.Trim => args(0)._2.asInstanceOf[String].trim
     case Library.UCase => args(0)._2.asInstanceOf[String].toUpperCase
     case Library.User => ""
+    case Library.Substring if args.size == 2 => 
+      args(0)._2.asInstanceOf[String].substring(args(1)._2.asInstanceOf[Int])
+    case Library.Substring if args.size == 3 => args(0)._2.asInstanceOf[String].
+      substring(args(1)._2.asInstanceOf[Int], args(2)._2.asInstanceOf[Int])
+    case Library.Replace => args(0)._2.asInstanceOf[String].
+      replace(args(1)._2.asInstanceOf[String], args(2)._2.asInstanceOf[String])
+    case Library.Reverse => args(0)._2.asInstanceOf[String].reverse
+    case Library.IndexOf => args(0)._2.asInstanceOf[String].indexOf(args(1)._2.asInstanceOf[String])
   }
 
   def unwrapSingleColumn(coll: Coll, tpe: Type): (Iterator[Any], Type) = tpe.asCollectionType.elementType match {
