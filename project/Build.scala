f54d272744e7b41bc2d3c258d7792a7e1e1a446b
Stefan Zeiger <szeiger@novocode.com>
2014-08-08 17:43:22 +0200

    Add BoneCP support to Database.forConfig.
    The configuration is very similar to Play’s. The new JdbcDataSource
    abstraction allows 3rd-party connection pools to be supported easily.
    JdbcBackend.Database gets a new method `close` for shutting down a
    connection pool.

--- project/Build.scala
+++ project/Build.scala
@@ -25,7 +25,8 @@ object SlickBuild extends Build {
     val slf4j = "org.slf4j" % "slf4j-api" % "1.6.4"
     val logback = "ch.qos.logback" % "logback-classic" % "0.9.28"
     val typesafeConfig = "com.typesafe" % "config" % "1.2.1"
-    val mainDependencies = Seq(slf4j, typesafeConfig)
+    val boneCP = "com.jolbox" % "bonecp" % "0.8.0.RELEASE"
+    val mainDependencies = Seq(slf4j, typesafeConfig, boneCP % "optional")
     val h2 = "com.h2database" % "h2" % "1.3.170"
     val testDBs = Seq(
       h2,
