e28a46e5f30934dc39c93508e2687d181dde925f
Stefan Zeiger <szeiger@novocode.com>
2015-07-15 18:26:17 +0200

    Cherry-picked DriverDataSource from master

---
+++ slick-testkit/src/test/scala/slick/test/jdbc/DataSourceTest.scala
@@ -0,0 +1,12 @@
+package slick.test.jdbc
+
+import org.junit.Test
+import org.junit.Assert._
+import slick.backend.DatabaseConfig
+import slick.driver.JdbcProfile
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class DataSourceTest {
+}
