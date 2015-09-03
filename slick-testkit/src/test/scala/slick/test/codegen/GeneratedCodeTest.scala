ea04bba596bfc74216b8e38093a51970ecee55a1
Naftoli Gugenheim <naftoligug@gmail.com>
2015-09-03 18:01:53 -0400

    codegen: Test case of no tables (schema has invalid syntax)
    It will just output `lazy val schema: profile.SchemaDescription = ` and stop

--- slick-testkit/src/test/scala/slick/test/codegen/GeneratedCodeTest.scala
+++ slick-testkit/src/test/scala/slick/test/codegen/GeneratedCodeTest.scala
@@ -133,6 +133,8 @@ object GeneratedCodeTest {
     )
   }
 
+  def testEmptyDB = slick.dbio.DBIO.successful(())
+
   def tableName( node:Node ) : String = {
     import slick.ast._
     node match {
