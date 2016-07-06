5dd4f31f2ea7fbb5de02589202c0933911aa9995
Minglei Tu <tmlneu@gmail.com>
2016-07-07 06:58:41 +0800

    Fixing tminglei/slick-pg#288 and slick/slick#966

--- slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/InsertTest.scala
+++ slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/InsertTest.scala
@@ -136,6 +136,28 @@ class InsertTest extends AsyncTest[JdbcTestDB] {
     } yield ()
   }
 
+  def testInsertOrUpdatePlainWithFuncDefinedPK = {
+    class T(tag: Tag) extends Table[(Int, String)](tag, "t_merge3") {
+      def id = column[Int]("id")
+      def name = column[String]("name")
+      def * = (id, name)
+      def ins = (id, name)
+      def pk = primaryKey("t_merge_pk_a", id)
+    }
+    val ts = TableQuery[T]
+
+    val upsertSql = tdb.profile.compileInsert(ts.toNode).upsert.sql
+    println(s">>> upsert sql: $upsertSql")
+
+    for {
+      _ <- ts.schema.create
+      _ <- ts ++= Seq((1, "a"), (2, "b"))
+      _ <- ts.insertOrUpdate((3, "c")).map(_ shouldBe 1)
+      _ <- ts.insertOrUpdate((1, "d")).map(_ shouldBe 1)
+      _ <- ts.sortBy(_.id).result.map(_ shouldBe Seq((1, "d"), (2, "b"), (3, "c")))
+    } yield ()
+  }
+
   def testInsertOrUpdateAutoInc = {
     class T(tag: Tag) extends Table[(Int, String)](tag, "T_MERGE2") {
       def id = column[Int]("ID", O.AutoInc, O.PrimaryKey)
