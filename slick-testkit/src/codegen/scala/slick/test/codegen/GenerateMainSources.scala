e53c400c0edf4fb24f55009564e2885ecb7e2712
AtkinsChang <atkinschang@gmail.com>
2016-08-01 15:53:48 +0800

    Fix compile error if postgres is not enable in test-dbs

--- slick-testkit/src/codegen/scala/slick/test/codegen/GenerateMainSources.scala
+++ slick-testkit/src/codegen/scala/slick/test/codegen/GenerateMainSources.scala
@@ -104,7 +104,32 @@ val  SimpleA = CustomTyping.SimpleA
         """.stripMargin
     },
     new UUIDConfig("Postgres2", StandardTestDBs.Postgres, "Postgres", Seq("/dbs/uuid-postgres.sql")),
-    new Config("Postgres3", StandardTestDBs.Postgres, "Postgres", Seq("/dbs/postgres.sql")),
+    new Config("Postgres3", StandardTestDBs.Postgres, "Postgres", Seq("/dbs/postgres.sql")) {
+      override def testCode: String =
+      """import slick.ast.{FieldSymbol, Select}
+        |import slick.jdbc.meta.MTable
+        |import slick.relational.RelationalProfile
+        |DBIO.seq(
+        |  schema.create,
+        |  MTable.getTables(Some(""), Some("public"), None, None).map { tables =>
+        |    def optionsOfColumn(c: slick.lifted.Rep[_]) =
+        |      c.toNode.asInstanceOf[Select].field.asInstanceOf[FieldSymbol].options.toList
+        |    val smallserialOptions = optionsOfColumn(TestDefault.baseTableRow.smallintAutoInc)
+        |    val serialOptions = optionsOfColumn(TestDefault.baseTableRow.intAutoInc)
+        |    val bigserialOptions = optionsOfColumn(TestDefault.baseTableRow.bigintAutoInc)
+        |    val char1EmptyOptions = optionsOfColumn(TestDefault.baseTableRow.char1DefaultEmpty)
+        |    val char1ValidOptions = optionsOfColumn(TestDefault.baseTableRow.char1DefaultValid)
+        |    val char1InvalidOptions = optionsOfColumn(TestDefault.baseTableRow.char1DefaultInvalid)
+        |    assertTrue("smallint_auto_inc should be AutoInc", smallserialOptions.exists(option => (option equals TestDefault.baseTableRow.O.AutoInc)))
+        |    assertTrue("int_auto_inc should be AutoInc", serialOptions.exists(option => (option equals TestDefault.baseTableRow.O.AutoInc)))
+        |    assertTrue("bigint_auto_inc should be AutoInc", bigserialOptions.exists(option => (option equals TestDefault.baseTableRow.O.AutoInc)))
+        |    assertTrue("default value of char1_default_empty should be ' '", char1EmptyOptions.exists(option => (option equals TestDefault.baseTableRow.O.Default(Some(' ')))))
+        |    assertTrue("default value of char1_default_valid should be 'a'", char1ValidOptions.exists(option => (option equals TestDefault.baseTableRow.O.Default(Some('a')))))
+        |    assertTrue("default value of char1_default_invalid should not exist", char1InvalidOptions.forall(option => (option.isInstanceOf[RelationalProfile.ColumnOption.Default[_]])))
+        |  }
+        |)
+      """.stripMargin
+    },
     new Config("EmptyDB", StandardTestDBs.H2Mem, "H2Mem", Nil),
     new Config("Oracle1", StandardTestDBs.Oracle, "Oracle", Seq("/dbs/oracle1.sql")) {
       override def useSingleLineStatements = true
