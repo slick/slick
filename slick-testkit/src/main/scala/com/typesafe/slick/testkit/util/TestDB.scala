07eb427b6b9b36020760e0040b583a5e3059e1d6
Yasuhiro TATSUNO <tatsuno_yasuhiro@kurusugawa.jp>
2018-10-20 15:15:20 +0900

    Java 9+ compatible Class Loading

--- slick-testkit/src/main/scala/com/typesafe/slick/testkit/util/TestDB.scala
+++ slick-testkit/src/main/scala/com/typesafe/slick/testkit/util/TestDB.scala
@@ -266,22 +266,9 @@ object ExternalTestDB {
   private[this] val driverCache = new mutable.HashMap[(String, String), Driver]()
 
   def getCustomDriver(url: String, driverClass: String) = synchronized {
-    val sysloader = java.lang.ClassLoader.getSystemClassLoader
-    val sysclass = classOf[URLClassLoader]
-
-    // Add the supplied jar onto the system classpath
-    // Doing this allows Hikari to initialise the driver, if needed
-    try {
-        val method = sysclass.getDeclaredMethod("addURL", classOf[URL])
-        method.setAccessible(true)
-        method.invoke(sysloader, new URL(url))
-    } catch {
-      case t: Throwable =>
-        t.printStackTrace()
-        throw new IOException(s"Error, could not add URL $url to system classloader");
-    }
-    driverCache.getOrElseUpdate((url, driverClass),
-      new URLClassLoader(Array(new URL(url)), getClass.getClassLoader).loadClass(driverClass).getConstructor().newInstance().asInstanceOf[Driver]
-    )
+    val urls = Array(new URL(url))
+    val loader = URLClassLoader.newInstance(urls, getClass.getClassLoader)
+    val driverKlass = Class.forName(driverClass, true, loader)
+    driverCache.getOrElseUpdate((url, driverClass), driverKlass.getConstructor().newInstance().asInstanceOf[Driver])
   }
 }
