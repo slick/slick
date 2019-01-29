36e99f8cc23326ebe71c05e878331e1dd034f6d2
jkugiya <j.kugiya@gmail.com>
2019-01-29 23:39:42 +0900

    issue #1627 Uses insert ignore for upsert. (#1879)

---
+++ slick-testkit/src/test/scala/slick/test/lifted/MySQLDMLTest.scala
@@ -0,0 +1,27 @@
+package slick.test.lifted
+
+import org.junit.Test
+import org.junit.Assert._
+
+class MySQLDMLTest {
+  import slick.jdbc.MySQLProfile.api._
+
+  @Test def testInsertOrUpdate(): Unit = {
+    class T(tag: Tag) extends Table[Int](tag, "mytable") {
+      def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+      def * = id
+    }
+    case class V(id: String, value: String)
+    class T2(tag: Tag) extends Table[V](tag, "mytable2") {
+      def id = column[String]("id", O.PrimaryKey)
+      def value = column[String]("value")
+      def * = (id, value) <> ((V.apply _).tupled, V.unapply)
+    }
+    assertTrue("generates insert ignore statements",
+      TableQuery[T].insertOrUpdate(1).statements.mkString.startsWith("insert ignore"))
+    assertTrue("there are no update action if insert primary key only",
+      TableQuery[T].insertOrUpdate(1).statements.mkString.endsWith(")"))
+    assertTrue("generates query with update action if insert with value",
+      TableQuery[T2].insertOrUpdate(V("test", "test-value")).statements.mkString.endsWith("on duplicate key update `value`=VALUES(`value`)"))
+  }
+}
