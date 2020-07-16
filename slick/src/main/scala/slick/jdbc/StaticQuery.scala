33312de8ee273a382bce90f15b6c7518b9c5df68
Stefan Zeiger <szeiger@novocode.com>
2020-07-16 19:17:57 +0200

    Remove macros from Plain SQL interpolators
    This sacrifices `stripMargin` and `tsql`. The former could certainly be
    reimplmenented (but the current implementation is based on the
    macro-based interpolators so it needs to be redone from scratch), the
    latter might also be possible with Dotty macros.

--- slick/src/main/scala/slick/jdbc/StaticQuery.scala
+++ slick/src/main/scala/slick/jdbc/StaticQuery.scala
@@ -2,64 +2,56 @@ package slick.jdbc
 
 import java.sql.PreparedStatement
 
-import scala.collection.mutable.ArrayBuffer
-import scala.language.implicitConversions
-
 import slick.dbio.Effect
 import slick.sql.SqlStreamingAction
 
+import scala.collection.mutable.ArrayBuffer
 
 class ActionBasedSQLInterpolation(val s: StringContext) extends AnyVal {
   /** Build a SQLActionBuilder via string interpolation */
-  def sql(params: TypedParameter[?]*): SQLActionBuilder =
-    SQLActionBuilder(s.parts, params)
-
+  def sql[P](param: P)(implicit pconv: SetParameter[P]): SQLActionBuilder[P] =
+    new SQLActionBuilder[P](s.parts, param, pconv)
   /** Build an Action for an UPDATE statement via string interpolation */
-  def sqlu(params: TypedParameter[?]*) = sql(params *).asUpdate
-}
-
-class TypedParameter[T](val param: T, val setParameter: SetParameter[T])
-
-object TypedParameter {
-  implicit def typedParameter[T](param: T)(implicit setParameter: SetParameter[T]): TypedParameter[T] =
-    new TypedParameter[T](param, setParameter)
+  def sqlu[P](param: P)(implicit pconv: SetParameter[P]) = sql(param).asUpdate
 }
 
 object SQLInterpolation {
-  def parse(strings: Seq[String], typedParams: Seq[TypedParameter[Any]]): (String, SetParameter[Unit]) = {
-    if (strings.length == 1)
-      (strings.head, SetParameter.SetUnit)
+  def parse[P](strings: Seq[String], param: P, pconv: SetParameter[P]): (String, SetParameter[Unit]) = {
+    if(strings.length == 1) (strings(0), SetParameter.SetUnit)
     else {
+      val (convs, params) = pconv match {
+        case pconv: SetTupleParameter[_] =>
+          (pconv.children.iterator, param.asInstanceOf[Product].productIterator)
+        case _ => (Iterator(pconv), Iterator(param))
+      }
       val b = new StringBuilder
       val remaining = new ArrayBuffer[SetParameter[Unit]]
-      typedParams.zip(strings.iterator).foreach { zipped =>
-        val p = zipped._1.param
+      convs.zip(params).zip(strings.iterator).foreach { zipped =>
+        val p = zipped._1._2
         var literal = false
         def decode(s: String): String =
-          if (s.endsWith("##"))
-            decode(s.substring(0, s.length - 2)) + "#"
-          else if (s.endsWith("#")) {
-            literal = true
-            s.substring(0, s.length - 1)
-          }
-          else
-            s
+          if(s.endsWith("##")) decode(s.substring(0, s.length-2)) + "#"
+          else if(s.endsWith("#")) { literal = true; s.substring(0, s.length-1) }
+          else s
         b.append(decode(zipped._2))
-        if (literal) b.append(p.toString)
+        if(literal) b.append(p.toString)
         else {
           b.append('?')
-          remaining += zipped._1.setParameter.applied(p)
+          remaining += zipped._1._1.asInstanceOf[SetParameter[Any]].applied(p)
         }
       }
       b.append(strings.last)
-      (b.toString, (u, pp) => remaining.foreach(_.apply(u, pp)))
+      (b.toString, new SetParameter[Unit] {
+        def apply(u: Unit, pp: PositionedParameters): Unit =
+          remaining.foreach(_.apply(u, pp))
+      })
     }
   }
 }
 
-case class SQLActionBuilder(strings: Seq[String], params: Seq[TypedParameter[?]]) {
-  def as[R](implicit getResult: GetResult[R]): SqlStreamingAction[Vector[R], R, Effect] = {
-    val (sql, unitPConv) = SQLInterpolation.parse(strings, params.asInstanceOf[Seq[TypedParameter[Any]]])
+case class SQLActionBuilder[P](strings: Seq[String], param: P, pconv: SetParameter[P]) {
+  def as[R](implicit rconv: GetResult[R]): SqlStreamingAction[Vector[R], R, Effect] = {
+    val (sql, unitPConv) = SQLInterpolation.parse(strings, param, pconv)
     new StreamingInvokerAction[Vector[R], R, Effect] {
       def statements = List(sql)
       protected[this] def createInvoker(statements: Iterable[String]) = new StatementInvoker[R] {
