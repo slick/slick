0480a3dac028eab422a9211e443cec5482ea7a86
Stefan Zeiger <szeiger@novocode.com>
2020-07-17 23:30:25 +0200

    Remove empty {} from some package objects

--- slick/src/main/scala/slick/jdbc/package.scala
+++ slick/src/main/scala/slick/jdbc/package.scala
@@ -2,5 +2,4 @@ package slick
 
 /** Contains the abstract `JdbcProfile` and related code. This includes all JDBC-related code,
   * facilities for <em>Plain SQL</em> queries, and JDBC-specific profile components. */
-package object jdbc {
-}
+package object jdbc
