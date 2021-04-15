c8c3b9e4f1efbf25813ddf70d17f4c81d98c7dc1
jkugiya <j.kugiya@gmail.com>
2021-04-16 01:39:32 +0900

    fix: MySql upsert (picked #2101) (#2201)
    * fix: MySql upsert (picked #2101)
    
    * Adds InsertTest for insertOrUpdate
    
    * chore: Added deprecated annotation
    
    * Update slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/InsertTest.scala
    
    Co-authored-by: nafg <98384+nafg@users.noreply.github.com>
    
    * Update slick/src/main/scala/slick/jdbc/MySQLProfile.scala
    
    Co-authored-by: nafg <98384+nafg@users.noreply.github.com>
    
    * fix tests
    
    * Update slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/InsertTest.scala
    
    Co-authored-by: nafg <98384+nafg@users.noreply.github.com>
    
    * Uses pattern match
    
    Co-authored-by: nafg <98384+nafg@users.noreply.github.com>

--- slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/InsertTest.scala
+++ slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/InsertTest.scala
@@ -56,7 +56,7 @@ class InsertTest extends AsyncTest[JdbcTestDB] {
     )
   }
 
-  
+
   def testUniqueInsert = {
     case class ARow(email: String , id: Int = 0)
     class A(tag: Tag) extends Table[ARow](tag , "A_UNIQUEINSERT"){
@@ -205,7 +205,7 @@ class InsertTest extends AsyncTest[JdbcTestDB] {
     } yield ()
   }
 
-  def testInsertOrUpdateAutoInc = {
+  def testInsertOrUpdateAutoInc: DBIOAction[Unit, NoStream, Effect.All] = {
     class T(tag: Tag) extends Table[(Int, String)](tag, "T_MERGE2") {
       def id = column[Int]("ID", O.AutoInc, O.PrimaryKey)
       def name = column[String]("NAME")
@@ -230,4 +230,96 @@ class InsertTest extends AsyncTest[JdbcTestDB] {
       }
     } yield ()).withPinnedSession
   }
+
+  // Regression test for https://github.com/slick/slick/issues/1627
+  def testInsertOrUpdateWithPrimaryKeyOnly: DBIOAction[Unit, NoStream, Effect.All] = tdb.confName match {
+    case "postgres" | "derbymem" | "derbydisk" =>
+      // TODO DerbyProfile and PostgresProfile seems to have a problem in this case, so we skip testing them.
+      // See https://github.com/slick/slick/issues/2206 and https://github.com/slick/slick/issues/2207
+      DBIO.seq()
+    case _ =>
+      class T(tag: Tag) extends Table[Int](tag, "mytable") {
+        def id = column[Int]("id", O.PrimaryKey)
+        def * = id
+      }
+      case class V(id: Int, value: String)
+      class T2(tag: Tag) extends Table[V](tag, "mytable2") {
+        def id = column[Int]("id")
+        def value = column[String]("value", O.Length(20))
+        def * = (id, value) <> ((V.apply _).tupled, V.unapply)
+        def pk = primaryKey("pk", (id, value))
+      }
+      val ts1 = TableQuery[T]
+      val ts2 = TableQuery[T2]
+      (for {
+        _ <- ts1.schema.create
+        _ <- ts1 ++= Seq((1), (2))
+        _ <- ts1.insertOrUpdate((0)).map(_ shouldBe 1)
+        _ <- ts1.insertOrUpdate((1)).map(_ shouldBe 1)
+        _ <- ts2.schema.create
+        _ <- ts2 ++= Seq(V(1, "a"), V(2, "b"))
+        _ <- ts2.insertOrUpdate(V(0, "c")).map(_ shouldBe 1)
+        _ <- ts2.insertOrUpdate(V(1, "a")).map(_ shouldBe 1)
+        _ <- ts2.insertOrUpdate(V(2, "d")).map(_ shouldBe 1)
+      } yield ()).withPinnedSession
+  }
+
+  // Regression test for https://github.com/slick/slick/issues/2045
+  def testInsertOrUpdateWithIntegrityError: DBIOAction[Unit, NoStream, Effect.All] = {
+    case class Book(id: Long, title: String, code: Option[String] = None)
+    class BooksTable(tag: Tag) extends Table[Book](tag, "books") {
+      def id = column[Long]("id", O.PrimaryKey)
+      def title = column[String]("title", O.Length(20))
+      def code = column[Option[String]]("code", O.Length(20))
+      override def * = (id, title, code) <> ((Book.apply _).tupled, Book.unapply)
+    }
+    val books = TableQuery[BooksTable]
+    case class BookMeta(id: Long, bookId: Long, tag: Long)
+    class BookMetaTable(tag: Tag) extends Table[BookMeta](tag, "book_meta") {
+      def id    = column[Long]("id")
+      def bookId = column[Long]("book_id")
+      def bookTag  = column[Long]("tag")
+      def pk = primaryKey("book_meta_pk", (bookId, id))
+      def book = foreignKey("book_fk", bookId, books)(_.id, onUpdate=ForeignKeyAction.Restrict, onDelete=ForeignKeyAction.Cascade)
+      override def * = (bookId, id, bookTag) <> ((BookMeta.apply _).tupled, BookMeta.unapply)
+    }
+    val meta = TableQuery[BookMetaTable]
+
+    import scala.util.{Success, Failure}
+    val bookMeta = BookMeta(-1, -1, 0) // fail, because there are no books.
+    DBIO.seq(
+      (meta.insertOrUpdate(bookMeta)).asTry.map {
+        case Success(_) => throw new Exception("Insertion should be failed.")
+        case _ => ()
+      }
+    )
+  }
+
+  // sqlite donesn't care this case
+  def testInsertAndUpdateShouldNotTruncateData = if (!tdb.confName.contains("sqlite")) {
+    case class Test(id: Int, name: String)
+    class TestTable(tag: Tag) extends Table[Test](tag, "test") {
+      def id = column[Int]("id", O.PrimaryKey)
+      def name = column[String]("name", O.Length(2))
+      def * = (id, name) <> (Test.tupled, Test.unapply)
+    }
+    val ts = TableQuery[TestTable]
+    import scala.util.{Success, Failure}
+    DBIO.seq(
+      ts.schema.create,
+      ts ++= Seq(Test(1, "a")),
+      (ts ++= Seq(Test(2, "123"))).asTry.map {
+        case Success(_) => throw new Exception("Data is truncated. It shouldn't be succeeded.")
+        case _ => ()
+      },
+      ts.filter(_.id === 1).map(_.name).update("123").asTry.map {
+        case Success(_) => throw new Exception("Data is truncated. It shouldn't be succeeded.")
+        case _ => ()
+      },
+      ts.insertOrUpdate(Test(1, "123")).asTry.map {
+        case Success(_) => throw new Exception("Data is truncated. It shouldn't be succeeded.")
+        case _ => ()
+      }
+    )
+  } else DBIO.seq()
 }
