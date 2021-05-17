61f7c2a013577b13aecb73f32018f5f7d55f59a7
Scala Steward <me@scala-steward.org>
2021-05-17 14:16:22 +0200

    Update scala-compiler, scala-library to 2.13.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   // NOTE: remember to change the version numbers in the sample projects
   // when changing them here
 
-  val scalaVersions = Seq("2.12.13", "2.13.5") // When updating these also update .travis.yml and appveyor.yml
+  val scalaVersions = Seq("2.12.13", "2.13.6") // When updating these also update .travis.yml and appveyor.yml
 
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.30"
   val typesafeConfig = "com.typesafe" % "config" % "1.4.1"
