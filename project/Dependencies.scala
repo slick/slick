6c0d469f7b4a9a3a0de4494ad6ffc29933dc3bb2
Scala Steward <me@scala-steward.org>
2021-07-06 19:08:18 +0200

    Update postgresql to 42.2.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     "org.apache.derby" % "derby" % "10.14.2.0",
     "org.xerial" % "sqlite-jdbc" % "3.36.0.1",
     "org.hsqldb" % "hsqldb" % "2.5.2",
-    "org.postgresql" % "postgresql" % "42.2.22",
+    "org.postgresql" % "postgresql" % "42.2.23",
     "mysql" % "mysql-connector-java" % "8.0.25",
     "net.sourceforge.jtds" % "jtds" % "1.3.1",
     "com.oracle.database.jdbc.debug" % "ojdbc8_g" % "21.1.0.0"
