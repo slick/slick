fcac2240de1633fb3c26146209f91b22d7c1da6b
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2021-07-14 01:50:40 +0200

    Provided a fix for CockroachDB issue with open portals (issue #2144) (#2145)
    * Provided a fix for CockroachDB issue with open portals
    
    * make the default argument for fetchSizeOverride a None as we usually don't want to override it

--- slick/src/main/scala/slick/jdbc/JdbcBackend.scala
+++ slick/src/main/scala/slick/jdbc/JdbcBackend.scala
@@ -365,7 +365,8 @@ trait JdbcBackend extends RelationalBackend {
     final def prepareStatement(sql: String,
                                defaultType: ResultSetType = ResultSetType.ForwardOnly,
                                defaultConcurrency: ResultSetConcurrency = ResultSetConcurrency.ReadOnly,
-                               defaultHoldability: ResultSetHoldability = ResultSetHoldability.Default): PreparedStatement = {
+                               defaultHoldability: ResultSetHoldability = ResultSetHoldability.Default,
+                               fetchSizeOverride: Option[Int] = None): PreparedStatement = {
       JdbcBackend.logStatement("Preparing statement", sql)
       val s = loggingPreparedStatement(decorateStatement(resultSetHoldability.withDefault(defaultHoldability) match {
         case ResultSetHoldability.Default =>
@@ -380,7 +381,8 @@ trait JdbcBackend extends RelationalBackend {
             resultSetConcurrency.withDefault(defaultConcurrency).intValue,
             h.intValue)
       }))
-      if(fetchSize != 0) s.setFetchSize(fetchSize)
+      val computedFetchSize = fetchSizeOverride.getOrElse(fetchSize)
+      if(computedFetchSize != 0) s.setFetchSize(computedFetchSize)
       s
     }
 
