e5d713c6e0a528c1e370332fb1a2ef76b7aede3e
Scala Steward <me@scala-steward.org>
2021-08-30 13:08:46 +0200

    Update sqlite-jdbc to 3.36.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
     h2,
     sqlServer,
     "org.apache.derby" % "derby" % "10.14.2.0",
-    "org.xerial" % "sqlite-jdbc" % "3.36.0.2",
+    "org.xerial" % "sqlite-jdbc" % "3.36.0.3",
     "org.hsqldb" % "hsqldb" % "2.5.2",
     "org.postgresql" % "postgresql" % "42.2.23",
     "mysql" % "mysql-connector-java" % "8.0.26",
