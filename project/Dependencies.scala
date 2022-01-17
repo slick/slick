8c019e1cd3544194e4d0f2d4e6f8eae720173ce7
Scala Steward <me@scala-steward.org>
2022-01-17 22:15:51 +0100

    Update mysql-connector-java to 8.0.28

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
     "org.xerial" % "sqlite-jdbc" % "3.36.0.3",
     "org.hsqldb" % "hsqldb" % "2.5.2",
     "org.postgresql" % "postgresql" % "42.3.1",
-    "mysql" % "mysql-connector-java" % "8.0.27",
+    "mysql" % "mysql-connector-java" % "8.0.28",
     "net.sourceforge.jtds" % "jtds" % "1.3.1",
     "com.oracle.database.jdbc.debug" % "ojdbc8_g" % "21.4.0.0.1"
   )
