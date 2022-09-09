8fed798bae9876cd35e9cf4a3584218004e55682
Scala Steward <scala_steward@virtuslab.com>
2022-09-09 18:47:54 +0000

    Update sqlite-jdbc to 3.39.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
     h2,
     sqlServer,
     "org.apache.derby" % "derby" % "10.14.2.0",
-    "org.xerial" % "sqlite-jdbc" % "3.39.2.1",
+    "org.xerial" % "sqlite-jdbc" % "3.39.3.0",
     "org.hsqldb" % "hsqldb" % "2.5.2",
     "org.postgresql" % "postgresql" % "42.5.0",
     "mysql" % "mysql-connector-java" % "8.0.30",
