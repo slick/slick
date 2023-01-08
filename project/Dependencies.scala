03bf700905d9a8abd13c2ece91a55550f0eb56b1
Scala Steward <scala_steward@virtuslab.com>
2023-01-08 02:38:36 +0000

    Revert commit(s) aded0995

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     sqlServer,
     "org.apache.derby" % "derby" % "10.14.2.0",
     "org.xerial" % "sqlite-jdbc" % "3.40.0.0",
-    "org.hsqldb" % "hsqldb" % "2.7.1",
+    "org.hsqldb" % "hsqldb" % "2.5.2",
     "org.postgresql" % "postgresql" % "42.5.1",
     "mysql" % "mysql-connector-java" % "8.0.31",
     "net.sourceforge.jtds" % "jtds" % "1.3.1",
