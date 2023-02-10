b33412e3f4a20cce9162c6524ece5f9e3bb8c5b5
Scala Steward <scala_steward@virtuslab.com>
2023-02-10 22:31:10 +0000

    Update hsqldb to 2.7.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     sqlServer,
     "org.apache.derby" % "derby" % "10.14.2.0",
     "org.xerial" % "sqlite-jdbc" % "3.40.1.0",
-    "org.hsqldb" % "hsqldb" % "2.5.2",
+    "org.hsqldb" % "hsqldb" % "2.7.1",
     "org.postgresql" % "postgresql" % "42.5.3",
     "mysql" % "mysql-connector-java" % "8.0.32",
     "com.oracle.database.jdbc.debug" % "ojdbc8_g" % "21.8.0.0",
