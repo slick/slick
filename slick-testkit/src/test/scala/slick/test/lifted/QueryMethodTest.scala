21958b22e995ffe21f4427b981a40e4baf4171bc
nafg <98384+nafg@users.noreply.github.com>
2023-06-19 03:56:10 +0000

    #2701 Add conditional filterNot methods (filterNotIf/filterNotOpt) support (#2751) #2701 Add conditional filterNot methods (filterNotIf/filterNotOpt) support
    * Query.scala: fix formatting

--- slick-testkit/src/test/scala/slick/test/lifted/QueryMethodTest.scala
+++ slick-testkit/src/test/scala/slick/test/lifted/QueryMethodTest.scala
@@ -3,7 +3,7 @@ package slick.test.lifted
 import org.junit.Assert._
 import org.junit.Test
 
-/** Test cases for filterIf and filterOpt */
+/** Test cases for filterIf/o and filterOpt */
 class QueryMethodTest {
 
   @Test def testQueryMethod = {
@@ -43,5 +43,29 @@ class QueryMethodTest {
     println(s6)
     assertTrue("filterIf stacks", s6 endsWith """where ("myString" = 'stack') and ("myString" like '%yes' escape '^')""")
 
+
+    val s7 = ts.filterNotOpt(Option.empty[String])(_.myString === _).result.statements.head
+    println(s7)
+    assertTrue("filterNotOpt adds no condition when the given option is empty", s7 endsWith """from "myschema"."mytable"""")
+
+    val s8 = ts.filterNotOpt(Some("something"))(_.myString === _).result.statements.head
+    println(s8)
+    assertTrue("filterNotOpt adds a condition when the given option is present", s8 endsWith """where not ("myString" = 'something')""")
+
+    val s9 = ts.filterNotOpt(Some("something"))(_.myString === _).filterNotOpt(Option.empty[String])(_.myString like _).filterNotOpt(Some("something"))(_.myString startsWith _).result.statements.head
+    println(s9)
+    assertTrue("filterNotOpt stacks", s9 endsWith """where (not ("myString" = 'something')) and (not ("myString" like 'something%' escape '^'))""")
+
+    val s10 = ts.filterNotIf(false)(_.myString === "wonderful").result.statements.head
+    println(s10)
+    assertTrue("filterNotIf adds no condition when the given option is present", s10 endsWith """from "myschema"."mytable"""")
+
+    val s11 = ts.filterNotIf(true)(_.myString startsWith "foo").result.statements.head
+    println(s11)
+    assertTrue("filterNotIf adds a condition when the given option is present", s11 endsWith """where not ("myString" like 'foo%' escape '^')""")
+
+    val s12 = ts.filterNotIf(false)(_.myString endsWith "nope").filterNotIf(true)(_.myString === "stack").filterNotIf(true)(_.myString endsWith "yes").result.statements.head
+    println(s12)
+    assertTrue("filterNotIf stacks", s12 endsWith """where (not ("myString" = 'stack')) and (not ("myString" like '%yes' escape '^'))""")
   }
 }
