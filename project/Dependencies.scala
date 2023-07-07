709714e538c2539ab4f60b358280fdd32bea82b3
Scala Steward <scala_steward@virtuslab.com>
2023-07-07 02:02:08 +0000

    Update h2 to 2.2.220

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val logback = "ch.qos.logback" % "logback-classic" % "1.3.8"
   val hikariCP = "com.zaxxer" % "HikariCP" % "4.0.3"
 
-  val h2 = "com.h2database" % "h2" % "1.4.200"
+  val h2 = "com.h2database" % "h2" % "2.2.220"
   val sqlServer = {
     val javaVersion = System.getProperty("java.version")
     val jreVersionToUse = if (javaVersion.startsWith("11") || javaVersion.startsWith("12")) {
