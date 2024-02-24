f262a9edc97037a57023594a85c0a99eef49f5f3
Scala Steward <scala_steward@virtuslab.com>
2024-02-24 00:39:24 +0000

    Update ojdbc8_g to 21.13.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
     "org.hsqldb" % "hsqldb" % "2.5.2",
     "org.postgresql" % "postgresql" % "42.7.2",
     "com.mysql" % "mysql-connector-j" % "8.3.0",
-    "com.oracle.database.jdbc.debug" % "ojdbc8_g" % "21.11.0.0",
+    "com.oracle.database.jdbc.debug" % "ojdbc8_g" % "21.13.0.0",
     "com.ibm.db2.jcc" % "db2jcc" % "db2jcc4"
   )
 
