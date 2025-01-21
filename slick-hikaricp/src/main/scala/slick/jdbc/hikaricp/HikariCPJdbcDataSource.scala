99e0fa58fde79ae61e16fc2a037b54404437a3c5
amir.hakim <amir.hakim@innovapost.com>
2025-01-21 16:40:03 -0500

    Adding KeepAlive config to HikariCPJdbcDataSource. Fixes #3071

--- slick-hikaricp/src/main/scala/slick/jdbc/hikaricp/HikariCPJdbcDataSource.scala
+++ slick-hikaricp/src/main/scala/slick/jdbc/hikaricp/HikariCPJdbcDataSource.scala
@@ -56,6 +56,7 @@ object HikariCPJdbcDataSource extends JdbcDataSourceFactory {
     hconf.setMaximumPoolSize(c.getIntOpt("maximumPoolSize").orElse(c.getIntOpt("maxConnections")).getOrElse(numThreads))
     hconf.setMinimumIdle(c.getIntOpt("minimumIdle").orElse(c.getIntOpt("minConnections")).getOrElse(numThreads))
     hconf.setPoolName(c.getStringOr("poolName", name))
+    hconf.setKeepaliveTime(c.getLongOpt("keepAliveTime").getOrElse(1800000))
 
     // Infrequently used
 
